4bba48c7c00e18a49537d9bcd9bc70e2854dd7e7
sruthi-002 <89520373+sruthi-002@users.noreply.github.com>
2022-08-01 19:36:16 +0530

    Add files via upload

---
+++ Insertion.java
@@ -0,0 +1,36 @@
+﻿package com.company;
+
+import java.util.Scanner;
+
+class sorting
+{
+    public void s()
+    {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int i,j,temp;
+        int []a = new int[n];
+        for(i=0;i<n;i++)
+            a[i]=in.nextInt();
+        for(i=1;i<n;i++)
+        {
+            j=i-1;
+            int k=a[i];
+            while(k<a[j] && j>=0)
+            {
+                a[j+1]=a[j];
+                j--;
+            }
+            a[j+1]=k;
+        }
+        for(i=0;i<n;i++)
+        System.out.println(a[i]);
+    }
+}
+public class Main {
+
+    public static void main(String[] args) {
+        sorting s = new sorting();
+        s.s();
+    }
+}
